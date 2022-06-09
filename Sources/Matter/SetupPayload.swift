deb1dc1a4a0a0c0fa4e4b50d45868ef3215fef60
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-06-09 11:49:13 -0700

    Conform `SetupPayload` to `MutableReferenceConvertible`

--- Sources/Matter/SetupPayload.swift
+++ Sources/Matter/SetupPayload.swift
@@ -7,14 +7,68 @@
 
 import Foundation
 @_implementationOnly import CMatter
+@_implementationOnly import std.string
 
-public final class SetupPayload {
+public struct SetupPayload {
     
-    internal typealias CPPObject = chip.SetupPayload
+    internal var handle: MutableHandle<ReferenceType>
     
-    internal let cppObject: CPPObject
+    internal init(_ handle: MutableHandle<ReferenceType>) {
+        self.handle = handle
+    }
     
-    internal init(_ cppObject: CPPObject) {
-        self.cppObject = cppObject
+    public init() {
+        self.init(MutableHandle(adoptingReference: ReferenceType()))
+    }
+    
+    public var version: UInt8 {
+        handle.uncopiedReference().version
+    }
+    
+    public var serial: String {
+        get throws {
+            try handle.uncopiedReference().serial
+        }
+    }
+}
+
+extension SetupPayload: MutableReferenceConvertible {
+    
+    final class ReferenceType: CXXReference, Duplicatable {
+        
+        typealias CXXObject = chip.SetupPayload
+        
+        private(set) var cxxObject: CXXObject
+        
+        init() {
+            self.cxxObject = CXXObject()
+        }
+        
+        init(_ cxxObject: CXXObject) {
+            self.cxxObject = cxxObject
+        }
+        
+        func copy() -> ReferenceType {
+            let copy = ReferenceType()
+            //copy.cxxObject.version = self.version
+            return copy
+        }
+        
+        var version: UInt8 {
+            //get { cxxObject.version }
+            //set { cxxObject.version = newValue }
+            fatalError()
+        }
+        
+        var serial: String {
+            get throws {
+                var cxxString = std.string.init()
+                let error = cxxObject.getSerialNumber(&cxxString)
+                guard error.AsInteger() == 0 else {
+                    throw MatterError(error)
+                }
+                return String(cString: cxxString.c_str())
+            }
+        }
     }
 }
