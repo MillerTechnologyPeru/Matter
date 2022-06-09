6b57a829d782ae902f0ae5f2d48166c1acbb2136
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-06-09 03:17:45 -0700

    Added `MatterError`

--- Sources/Matter/Error.swift
+++ Sources/Matter/Error.swift
@@ -6,22 +6,130 @@
 //
 
 import Foundation
+@_implementationOnly import CMatter
 
 /// Matter framework error
-public enum MatterError: Error {
+public struct MatterError: Error {
     
-    /// Unknown error
-    case unknown
+    public typealias Code = MatterErrorCode
     
-    /// Invalid string length
-    case invalidStringLength
+    public typealias Range = MatterErrorRange
     
-    /// Interaction error code
-    case interaction(MatterInteractionErrorCode)
+    internal let cppObject: CHIP_ERROR
+    
+    internal init(_ cppObject: CHIP_ERROR) {
+        self.cppObject = cppObject
+    }
+    
+    public init(_ code: Code, range: Range, file: StaticString = #file, line: UInt = #line) {
+        let error = file.withUTF8Buffer { fileBuffer in
+            CHIP_ERROR(CHIP_ERROR.Range(range), code.rawValue, fileBuffer.baseAddress, numericCast(line))
+        }
+        self.init(error)
+    }
+    
+    public init(_ code: Code) {
+        self.init(CHIP_ERROR(code.rawValue))
+    }
+    
+    public var code: Code {
+        return Code(rawValue: cppObject.AsInteger())
+    }
+    
+    public var message: String {
+        return String(cString: cppObject.AsString())
+    }
 }
 
-/// Matter error code
-public enum MatterInteractionErrorCode: UInt8 {
+extension MatterError: CustomStringConvertible, CustomDebugStringConvertible {
     
-    case failure = 0x01
+    public var description: String {
+        return message
+    }
+    
+    public var debugDescription: String {
+        return message
+    }
+}
+
+// Use "hidden" entry points for `NSError` bridging
+extension MatterError {
+    
+    public var _code: Int { Int(code.rawValue) }
+
+    public var _domain: String { "MatterErrorDomain" }
+}
+
+// MARK: - Error Code
+
+public struct MatterErrorCode: RawRepresentable, Equatable, Hashable {
+    
+    public let rawValue: UInt32
+    
+    public init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+    
+    private init(_ raw: UInt32) {
+        self.init(rawValue: raw)
+    }
+}
+
+extension MatterErrorCode: ExpressibleByIntegerLiteral {
+    
+    public init(integerLiteral value: UInt32) {
+        self.init(rawValue: value)
+    }
+}
+
+public extension MatterError {
+    
+    /// A message exceeds the sent limit.
+    static var sendingBlocked: MatterErrorCode { 0x01 }
+}
+
+public extension MatterErrorCode {
+    
+    /// Match error code
+    static func ~= (lhs: MatterErrorCode, rhs: Error) -> Bool {
+        guard let value = rhs as? MatterError else { return false }
+        return value.code == lhs
+    }
+}
+
+// MARK: - Range
+
+public enum MatterErrorRange: UInt8, CaseIterable {
+    
+    /// CHIP SDK errors.
+    case sdk            = 0x0
+    
+    /// Encapsulated OS errors, other than POSIX errno.
+    case os             = 0x1
+    
+    /// Encapsulated POSIX errno values.
+    case posix          = 0x2
+    
+    /// Encapsulated LwIP errors.
+    case lwIP           = 0x3
+    
+    /// Encapsulated OpenThread errors.
+    case cpenThread     = 0x4
+    
+    /// Platform-defined encapsulation.
+    case platform       = 0x5
+}
+
+internal extension MatterErrorRange {
+    
+    init(_ range: CHIP_ERROR.Range) {
+        self.init(rawValue: range.rawValue)!
+    }
+}
+
+internal extension CHIP_ERROR.Range {
+    
+    init(_ range: MatterErrorRange) {
+        self.init(rawValue: range.rawValue)!
+    }
 }
--- Tests/MatterTests/MatterTests.swift
+++ Tests/MatterTests/MatterTests.swift
@@ -1,10 +1,26 @@
 import XCTest
 @testable import Matter
-@_implementationOnly import CMatter
+@testable import Example
 
 final class MatterTests: XCTestCase {
     
     func testExample() throws {
-        print("CHIP version: \(CHIP_VERSION_STRING)")
+        print("Matter version: \(MatterVersion)")
+        
+    }
+    
+    func testError() {
+        
+        let error = MatterError(MatterError.sendingBlocked)
+        
+        XCTAssertEqual(error.message, "Error 0xE5E08318")
+        
+        do {
+            throw error
+        } catch MatterError.sendingBlocked {
+            // caught error
+        } catch {
+            XCTFail("Did not catch error")
+        }
     }
 }
