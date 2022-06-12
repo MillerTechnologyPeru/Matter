d239ee1d8c6720b2fa5ae4301b54a98a10e130bd
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-06-11 19:01:11 -0700

    Updated documentation

--- Sources/Matter/Bluetooth/BluetoothUUID.swift
+++ Sources/Matter/Bluetooth/BluetoothUUID.swift
@@ -10,11 +10,15 @@ import Bluetooth
 
 public extension BluetoothUUID {
     
+    /// Matter Bluetooth Service UUID
     static var matterService: BluetoothUUID { BluetoothUUID(rawValue: "0000FFF6-0000-1000-8000-00805F9B34FB")! }
     
+    /// UUID of Matter service characteristic used for central writes.
     static var matterCharacteristic1: BluetoothUUID { BluetoothUUID(rawValue: "18EE2EF5-263D-4559-959F-4F9C429F9D11")! }
     
+    /// UUID of Matter service characteristic used for peripheral indications.
     static var matterCharacteristic2: BluetoothUUID { BluetoothUUID(rawValue: "18EE2EF5-263D-4559-959F-4F9C429F9D12")! }
     
+    /// UUID of Matter service characteristic used for additional data.
     static var matterCharacteristic3: BluetoothUUID { BluetoothUUID(rawValue: "64630238-8772-45F2-B87D-748A83218F04")! }
 }
