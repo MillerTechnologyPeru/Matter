79aff2d405eee9af345103c7d56547f3519b8f74
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-06 20:11:45 -0800

    Updated `ServiceDiscoveryManager`

--- Sources/Matter/ServiceDiscovery/ServiceDiscoveryManager.swift
+++ Sources/Matter/ServiceDiscovery/ServiceDiscoveryManager.swift
@@ -16,30 +16,127 @@ public protocol ServiceDiscoveryManager {
     func publish(service: ServiceDiscoveryService) async throws
 }
 
-public final class NetServiceManager: NSObject, ServiceDiscoveryManager {
+public final class NetServiceManager: ServiceDiscoveryManager {
 
-    public override init() { }
+    public init() {
+        #if DEBUG
+        self.log = { print($0) }
+        #endif
+        let _ = self.delegate
+    }
+    
+    public var log: ((String) -> ())?
+        
+    private lazy var delegate = Delegate(self)
+    
+    private let storage = Storage()
     
     public func publish(
         service: ServiceDiscoveryService
     ) async throws {
-        let netService = NetService(
-            domain: "local",
-            type: service.type + service.protocol.stringValue,
-            name: service.name,
-            port: Int32(service.port)
-        )
-        let txtRecord = NetService.data(fromTXTRecord: service.txtRecord)
-        netService.setTXTRecord(txtRecord)
-        netService.delegate = self
-        netService.publish()
+        let (object, publish) = await storage.update {
+            let netService = NetService(
+                domain: "local",
+                type: service.type + service.protocol.stringValue,
+                name: service.name,
+                port: Int32(service.port)
+            )
+            let txtRecord = NetService.data(fromTXTRecord: service.txtRecord)
+            netService.setTXTRecord(txtRecord)
+            netService.delegate = delegate
+            $0.services.insert(netService)
+            let publish = { netService.publish() }
+            let object = ObjectIdentifier(netService)
+            return (object, publish)
+        }
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Swift.Error>) in
+            Task {
+                await storage.update {
+                    $0.continuation.publish[object] = continuation
+                    publish() // execute
+                }
+            }
+        }
+    }
+}
+
+public extension NetServiceManager {
+    
+    enum Error: Swift.Error {
+        
+        case publish([String: NSNumber])
+    }
+}
+
+internal extension NetServiceManager {
+    
+    actor Storage {
+        
+        init() { }
+        
+        var state = InternalState()
+        
+        func update<T>(_ block: (inout InternalState) -> (T)) -> T {
+            return block(&state)
+        }
+    }
+    
+    struct InternalState {
+        
+        var continuation = Continuation()
+        
+        var services = Set<NetService>()
+    }
+    
+    struct Continuation {
+        
+        var publish = [ObjectIdentifier: CheckedContinuation<Void, Swift.Error>]()
     }
 }
 
 // MARK: - NetServiceDelegate
 
-extension NetServiceManager: NetServiceDelegate {
+extension NetServiceManager {
     
+    final class Delegate: NSObject, NetServiceDelegate {
+        
+        weak var manager: NetServiceManager!
+        
+        init(_ manager: NetServiceManager) {
+            self.manager = manager
+        }
+        
+        func log(_ message: String) {
+            self.manager?.log?(message)
+        }
+        
+        func netServiceWillPublish(_ sender: NetService) {
+            log("Will publish \(sender.domain).\(sender.type):\(sender.name)")
+        }
+        
+        func netServiceDidPublish(_ sender: NetService) {
+            log("Did publish \(sender.domain).\(sender.type):\(sender.name)")
+            let object = ObjectIdentifier(sender)
+            Task {
+                await manager.storage.update {
+                    $0.continuation.publish[object]?.resume()
+                    $0.continuation.publish[object] = nil
+                }
+            }
+        }
+        
+        func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+            log("Did not publish \(sender.domain).\(sender.type):\(sender.name)")
+            let error = NetServiceManager.Error.publish(errorDict)
+            let object = ObjectIdentifier(sender)
+            Task {
+                await manager.storage.update {
+                    $0.continuation.publish[object]?.resume(throwing: error)
+                    $0.continuation.publish[object] = nil
+                }
+            }
+        }
+    }
 }
 
 // MARK: - C Callbacks
